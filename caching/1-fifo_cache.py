b91ae06488ea0f9b5e3f45cd1dcd493191025586
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-18 15:31:42 +0200

    feat: implement FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Class that inherits from BaseCaching and is a caching system
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Define FIFOCache """
+
+    def __init__(self):
+        """ Initialize FIFOCache """
+        self.queue = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign the dictionary the item value """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+
+    def get(self, key):
+        """ Output the value associated with the given key """
+        return self.cache_data.get(key)
